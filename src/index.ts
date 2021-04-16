7ad2de988e0096da9986ffe25d167d3d1f45498b
Ognjen Bostjancic <ognjen@anyline.com>
2021-04-17 01:15:04 +0200

    Changed entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { driver as strategyDriver } from "./1-strategy";
-import { driver as observerDriver } from "./2-observer";
+import { driver as observerDriver } from "./2-observer/weather";
 import { driver as decoratorDriver } from "./3-decorator";
 import { driver as factoryDriver } from "./4-factory";
 
-strategyDriver();
-// observerDriver();
+// Favour composition over inheritance
+// Encapsulate what varies
+// Program to interfaces, not implementations
+// Strive for loosely coubled designs between objects that interact
+
+// strategyDriver();
+observerDriver();
 // decoratorDriver();
 // factoryDriver();
