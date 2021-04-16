88e36b5f96a750e7ad6bac0d061a448bf8b38d7a
Ognjen Bostjancic <ognjen@anyline.com>
2021-04-16 19:04:45 +0200

    Added entry point

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { driver as strategyDriver } from "./1-strategy";
+import { driver as observerDriver } from "./2-observer";
+import { driver as decoratorDriver } from "./3-decorator";
+import { driver as factoryDriver } from "./4-factory";
+
+strategyDriver();
+// observerDriver();
+// decoratorDriver();
+// factoryDriver();
