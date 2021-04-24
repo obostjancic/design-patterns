e9de59080d4535164680308e58c20ddeef210db4
Ognjen Bostjancic <ognjen@anyline.com>
2021-04-24 15:38:55 +0200

    Composute pattern cleanup

--- src/9-iterator-and-composite/diner-composite/iterator.ts
+++ src/9-iterator-and-composite/diner-composite/iterator.ts
@@ -37,14 +37,12 @@ export class CompositeIterator<T> implements Iterator<T> {
   public next(): T {
     if (this.hasNext()) {
       const iterator = this.stack.peek();
-      const component = iterator.next();
+      const component = iterator.next() as any;
 
-      //@ts-ignore
-      this.stack.push(component?.createIterator());
+      this.stack.push(component.createIterator());
 
       return component;
-      //@ts-ignore
-    } else return null;
+    } else throw Error();
   }
 
   public hasNext(): boolean {
@@ -64,66 +62,7 @@ export class NullIterator<T> implements Iterator<T> {
     return false;
   }
 
-  //@ts-ignore
   public next(): T {
     throw new Error('Unsupported operation');
   }
 }
-
-// export class DinerMenuIterator implements Iterator<MenuItem> {
-//   private items: Set<MenuItem>;
-//   private position: number = 0;
-
-//   constructor(items: Set<MenuItem>) {
-//     this.items = items;
-//   }
-
-//   hasNext(): boolean {
-//     return this.position < this.items.size;
-//   }
-
-//   next(): MenuItem {
-//     // probably not the best idea ever :D
-//     const res = Array.from(this.items)[this.position];
-//     this.position++;
-//     return res;
-//   }
-// }
-
-// export class PancakeMenuIterator implements Iterator<MenuItem> {
-//   private items: MenuItem[];
-//   private position: number = 0;
-
-//   constructor(items: MenuItem[]) {
-//     this.items = items;
-//   }
-
-//   hasNext(): boolean {
-//     return this.position < this.items.length;
-//   }
-
-//   next(): MenuItem {
-//     const result = this.items[this.position];
-//     this.position++;
-//     return result;
-//   }
-// }
-// export class CafeMenuIterator implements Iterator<MenuItem> {
-//   private items: Map<string, MenuItem>;
-//   private position: number = 0;
-
-//   constructor(items: Map<string, MenuItem>) {
-//     this.items = items;
-//   }
-
-//   hasNext(): boolean {
-//     return this.position < this.items.size;
-//   }
-
-//   next(): MenuItem {
-//     // probably not the best idea ever :D
-//     const res = Array.from(this.items)[this.position];
-//     this.position++;
-//     return res[1];
-//   }
-// }
